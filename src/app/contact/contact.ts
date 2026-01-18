ea2056473d536f48f09c3fbf40cce7d91ee7e16a
Armin Puffler <a.puffler@protonmail.com>
2026-01-18 15:10:03 +0100

    Added content to contact page and did styling

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   imports: [],
-  template: `
-    <p>
-      contact works!
-    </p>
-  `,
-  styles: ``,
+  templateUrl: `contact.html`,
+  styleUrl: "./contact.css",
 })
 export class Contact {
 
