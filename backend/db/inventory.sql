bbf24b986c58a6e819fef359686c06297760b16d
Armin Puffler <a.puffler@protonmail.com>
2026-06-22 18:08:26 +0200

    changed schema

--- backend/db/inventory.sql
+++ backend/db/inventory.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS items (
-    id SERIAL PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY,
     category VARCHAR(255),
     name VARCHAR(255) NOT NULL,
     description TEXT,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS items (
 );
 
 CREATE TABLE IF NOT EXISTS categories (
-    id SERIAL PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY,
     handle VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     description TEXT
@@ -25,18 +25,19 @@ CREATE TABLE IF NOT EXISTS categories (
             category: "Fishing Rods"
           }];
 */
-INSERT OR IGNORE INTO items (id, category, name, description, price, imageUrl, stock) VALUES
-(NULL, 'fishing-rods', 'Fox Horizon_X3_S', 'A high-quality fishing rod for all your angling needs.', 49.99, 'assets/images/Fox Horizon_X3_S.webp', 15),
-(NULL, 'fishing-reels', 'Fishing Reel', 'A durable fishing reel that provides smooth performance.', 29.99, 'assets/images/fishing-reel.jpg', 15),
-(NULL, 'fishing-lines', 'Fishing Line', 'Strong and reliable fishing line for various fishing conditions.', 9.99, 'assets/images/fishing-line.jpg', 50),
-(NULL, 'fishing-lures', 'Fishing Lure', 'A variety of fishing lures to attract different types of fish.', 14.99, 'assets/images/fishing-lure.jpg', 30),
-(NULL, 'baits-and-lures', 'Baits and Lures', 'A variety of baits and lures to attract different types of fish.', 14.99, 'assets/images/baits-and-lures.jpg', 30),
-(NULL, 'apparel', 'Apparel', 'Stay comfortable and protected with our fishing apparel collection.', 29.99, 'assets/images/apparel.jpg', 25);
+INSERT OR IGNORE INTO items (category, name, description, price, imageUrl, stock) VALUES
+('fishing-rods', 'Fox Horizon_X3_S', 'A high-quality fishing rod for all your angling needs.', 49.99, 'assets/images/Fox Horizon_X3_S.webp', 15),
+('fishing-rods', 'testrod', 'Testrod.',49.99, 'assets/images/Fox Horizon_X3_S.webp', 15),
+('fishing-reels', 'Fishing Reel', 'A durable fishing reel that provides smooth performance.', 29.99, 'assets/images/fishing-reel.jpg', 15),
+('fishing-lines', 'Fishing Line', 'Strong and reliable fishing line for various fishing conditions.', 9.99, 'assets/images/fishing-line.jpg', 50),
+('fishing-lures', 'Fishing Lure', 'A variety of fishing lures to attract different types of fish.', 14.99, 'assets/images/fishing-lure.jpg', 30),
+('baits-and-lures', 'Baits and Lures', 'A variety of baits and lures to attract different types of fish.', 14.99, 'assets/images/baits-and-lures.jpg', 30),
+('apparel', 'Apparel', 'Stay comfortable and protected with our fishing apparel collection.', 29.99, 'assets/images/apparel.jpg', 25);
 
-INSERT OR IGNORE INTO categories (id, handle, name, description) VALUES
-(NULL, 'fishing-rods', 'Fishing Rods', 'A wide selection of fishing rods for all skill levels.'),
-(NULL, 'fishing-reels', 'Fishing Reels', 'Durable and high-performance fishing reels.'),
-(NULL, 'fishing-lines', 'Fishing Lines', 'Strong and reliable fishing lines for various conditions.'),
-(NULL, 'fishing-lures', 'Fishing Lures', 'A variety of fishing lures to attract different types of fish.'),
-(NULL, 'baits-and-lures', 'Baits and Lures', 'A variety of baits and lures to attract different types of fish.'),
-(NULL, 'apparel', 'Apparel', 'Stay comfortable and protected with our fishing apparel collection.');
+INSERT OR IGNORE INTO categories (handle, name, description) VALUES
+('fishing-rods', 'Fishing Rods', 'A wide selection of fishing rods for all skill levels.'),
+('fishing-reels', 'Fishing Reels', 'Durable and high-performance fishing reels.'),
+('fishing-lines', 'Fishing Lines', 'Strong and reliable fishing lines for various conditions.'),
+('fishing-lures', 'Fishing Lures', 'A variety of fishing lures to attract different types of fish.'),
+('baits-and-lures', 'Baits and Lures', 'A variety of baits and lures to attract different types of fish.'),
+('apparel', 'Apparel', 'Stay comfortable and protected with our fishing apparel collection.');
